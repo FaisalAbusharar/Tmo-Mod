2c353608822e37d93edadcbd94970685525c9841
VoidyCD <pvadiscordteam@gmail.com>
2023-03-19 11:46:51 +0300

    forgot to add lang

--- src/main/java/com/voidy/tmo/mixin/TooManyOresMixin.java
+++ src/main/java/com/voidy/tmo/mixin/TooManyOresMixin.java
@@ -7,9 +7,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class ExampleMixin {
+public class TooManyOresMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
+		// love how this class is so useless yet the game wont start without it at the moment
 	}
 }
