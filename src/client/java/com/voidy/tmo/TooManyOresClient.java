aa808e2875b98e015a613862e2593638bf766223
VoidyCD <pvadiscordteam@gmail.com>
2023-03-19 10:33:35 +0300

    refactored files, setup project for development

--- src/client/java/com/voidy/tmo/TooManyOresClient.java
+++ src/client/java/com/voidy/tmo/TooManyOresClient.java
@@ -2,7 +2,8 @@ package com.voidy.tmo;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class TooManyOresClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
