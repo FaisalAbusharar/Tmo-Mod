4c7aa1daabe3af83e7c755b9f49c6b6de4f47262
VoidyCD <pvadiscordteam@gmail.com>
2023-03-19 18:50:38 +0300

    Citrine Ore now spawns naturally

--- src/main/java/com/voidy/tmo/world/ModConfiguredFeatures.java
+++ src/main/java/com/voidy/tmo/world/ModConfiguredFeatures.java
@@ -2,32 +2,46 @@ package com.voidy.tmo.world;
 
 
 
+
 import com.voidy.tmo.TooManyOresMain;
 import com.voidy.tmo.block.ModBlocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.rule.RuleTest;
+import net.minecraft.structure.rule.TagMatchRuleTest;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 
+import java.util.List;
+
 public class ModConfiguredFeatures {
     public static final RegistryKey<ConfiguredFeature<?, ?>> RED_MAPLE_KEY = registerKey("red_maple");
+    public static final RegistryKey<ConfiguredFeature<?, ?>> CITRINE_ORE_KEY = registerKey("citrine_ore");
 
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplaceables = new TagMatchRuleTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceables = new TagMatchRuleTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+
+        List<OreFeatureConfig.Target> overworldCitrineOres =
+                List.of(OreFeatureConfig.createTarget(stoneReplaceables, ModBlocks.CITRINE_ORE.getDefaultState()),
+                        OreFeatureConfig.createTarget(deepslateReplaceables, ModBlocks.DEEPSLATE_CITRINE_ORE.getDefaultState()));
+
         register(context, RED_MAPLE_KEY, Feature.TREE, new TreeFeatureConfig.Builder(
                 BlockStateProvider.of(ModBlocks.RED_MAPLE_LOG),
                 new StraightTrunkPlacer(5, 6, 3),
                 BlockStateProvider.of(ModBlocks.RED_MAPLE_LEAVES),
                 new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 7),
                 new TwoLayersFeatureSize(1, 0, 2)).build());
+
+
+        register(context, CITRINE_ORE_KEY, Feature.ORE, new OreFeatureConfig(overworldCitrineOres, 12));
     }
     // what the fuck is this method god help me please lord
 
--- src/main/java/com/voidy/tmo/world/gen/ModOreGeneration.java
+++ src/main/java/com/voidy/tmo/world/gen/ModOreGeneration.java
@@ -1,15 +1,15 @@
 package com.voidy.tmo.world.gen;
 
+
 import com.voidy.tmo.world.ModPlacedFeatures;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.world.biome.BiomeKeys;
+
 import net.minecraft.world.gen.GenerationStep;
 
-public class ModWorldGeneration {
-    public static void generateModWorldGen() {
-        ModTreeGeneration.generateTrees();
-        ModOreGeneration.generateOres();
+public class ModOreGeneration {
+    public static void generateOres() {
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.CITRINE_ORE_PLACED_KEY);
     }
-
 }
