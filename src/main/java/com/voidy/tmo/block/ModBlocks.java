913d0a087fd598cfe6c84cd845873b807051dca3
VoidyCD <pvadiscordteam@gmail.com>
2023-03-19 13:07:23 +0300

    added recipes/smelting/blasting

--- src/main/java/com/voidy/tmo/block/ModBlocks.java
+++ src/main/java/com/voidy/tmo/block/ModBlocks.java
@@ -3,7 +3,6 @@ package com.voidy.tmo.block;
 import com.voidy.tmo.TooManyOresMain;
 import com.voidy.tmo.items.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
