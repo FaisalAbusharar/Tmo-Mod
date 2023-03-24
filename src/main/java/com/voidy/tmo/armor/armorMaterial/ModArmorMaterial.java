3d012cdca9ffc59ed53c5d71eb5a4c51be9c64e2
VoidyCD <pvadiscordteam@gmail.com>
2023-03-24 14:18:39 +0300

    added Armor Material

---
+++ src/main/java/com/voidy/tmo/armor/armorMaterial/ModArmorMaterial.java
@@ -0,0 +1,108 @@
+package com.voidy.tmo.armor.armorMaterial;
+
+import java.util.EnumMap;
+import java.util.function.Supplier;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Lazy;
+import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.Util;
+
+public enum ModArmorMaterial implements StringIdentifiable, ArmorMaterial {
+
+    CITRINE("citrine", 15, Util.make(new EnumMap<ArmorItem.Type, Integer>(ArmorItem.Type.class), map -> {
+        map.put(ArmorItem.Type.BOOTS, 2);
+        map.put(ArmorItem.Type.LEGGINGS, 5);
+        map.put(ArmorItem.Type.CHESTPLATE, 6);
+        map.put(ArmorItem.Type.HELMET, 2);
+    }), 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0f, 0.0f, () -> Ingredient.ofItems(Items.EMERALD)),
+
+    EMERALD("emerald", 33, Util.make(new EnumMap<ArmorItem.Type, Integer>(ArmorItem.Type.class), map -> {
+        map.put(ArmorItem.Type.BOOTS, 3);
+        map.put(ArmorItem.Type.LEGGINGS, 6);
+        map.put(ArmorItem.Type.CHESTPLATE, 8);
+        map.put(ArmorItem.Type.HELMET, 3);
+    }), 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0f, 0.0f, () -> Ingredient.ofItems(Items.EMERALD));
+
+
+    public static final StringIdentifiable.Codec<net.minecraft.item.ArmorMaterials> CODEC;
+    private static final EnumMap<ArmorItem.Type, Integer> BASE_DURABILITY;
+    private final String name;
+    private final int durabilityMultiplier;
+    private final EnumMap<ArmorItem.Type, Integer> protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Lazy<Ingredient> repairIngredientSupplier;
+
+    private ModArmorMaterial(String name, int durabilityMultiplier, EnumMap<ArmorItem.Type, Integer> protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredientSupplier) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredientSupplier = new Lazy<Ingredient>(repairIngredientSupplier);
+    }
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return BASE_DURABILITY.get((Object) type) * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return this.protectionAmounts.get((Object) type);
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredientSupplier.get();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+
+    @Override
+    public String asString() {
+        return this.name;
+    }
+
+    static {
+        CODEC = StringIdentifiable.createCodec(net.minecraft.item.ArmorMaterials::values);
+        BASE_DURABILITY = Util.make(new EnumMap(ArmorItem.Type.class), map -> {
+            map.put(ArmorItem.Type.BOOTS, 13);
+            map.put(ArmorItem.Type.LEGGINGS, 15);
+            map.put(ArmorItem.Type.CHESTPLATE, 16);
+            map.put(ArmorItem.Type.HELMET, 11);
+        });
+    }
+}
