2bc037f5147ddd16bdb7ad37f2919e69e1771570
VoidyCD <pvadiscordteam@gmail.com>
2023-03-24 14:03:11 +0300

    made code smaller

--- src/main/java/com/voidy/tmo/data/ModRecipeGenerator.java
+++ src/main/java/com/voidy/tmo/data/ModRecipeGenerator.java
@@ -36,103 +36,41 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     offerShapelessRecipe(exporter, ModBlocks.RED_MAPLE_PLANKS, ModBlocks.RED_MAPLE_WOOD, "BUILD_BLOCKS",4);
 
 
+        //EMERALD
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_PICKAXE).pattern("SSS").pattern(" C ").pattern(" C ").input('S', Items.EMERALD).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(Items.EMERALD), FabricRecipeProvider.conditionsFromItem(Items.EMERALD)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_PICKAXE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_SHOVEL).pattern(" S ").pattern(" C ").pattern(" C ").input('S', Items.EMERALD).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(Items.EMERALD), FabricRecipeProvider.conditionsFromItem(Items.EMERALD)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_SHOVEL)));
 
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_PICKAXE)
-                .pattern("SSS")
-                .pattern(" C ")
-                .pattern(" C ")
-                .input('S', Items.EMERALD)
-                .input('C', Items.STICK)
-                .criterion(FabricRecipeProvider.hasItem(Items.EMERALD),
-                        FabricRecipeProvider.conditionsFromItem(Items.EMERALD))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_PICKAXE)));
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_SHOVEL)
-                .pattern(" S ")
-                .pattern(" C ")
-                .pattern(" C ")
-                .input('S', Items.EMERALD)
-                .input('C', Items.STICK)
-                .criterion(FabricRecipeProvider.hasItem(Items.EMERALD),
-                        FabricRecipeProvider.conditionsFromItem(Items.EMERALD))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_SHOVEL)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_AXE).pattern("SS ").pattern("SC ").pattern(" C ").input('S', Items.EMERALD).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(Items.EMERALD), FabricRecipeProvider.conditionsFromItem(Items.EMERALD)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_AXE)));
 
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_AXE)
-                .pattern("SS ")
-                .pattern("SC ")
-                .pattern(" C ")
-                .input('S', Items.EMERALD)
-                .input('C', Items.STICK)
-
-                .criterion(FabricRecipeProvider.hasItem(Items.EMERALD),
-                        FabricRecipeProvider.conditionsFromItem(Items.EMERALD))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_AXE)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_SWORD).pattern(" S ").pattern(" S ").pattern(" C ").input('S', Items.EMERALD).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(Items.EMERALD), FabricRecipeProvider.conditionsFromItem(Items.EMERALD)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_SWORD)));
 
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_SWORD)
-                .pattern(" S ")
-                .pattern(" S ")
-                .pattern(" C ")
-                .input('S', Items.EMERALD)
-                .input('C', Items.STICK)
-                .criterion(FabricRecipeProvider.hasItem(Items.EMERALD),
-                        FabricRecipeProvider.conditionsFromItem(Items.EMERALD))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_SWORD)));
-
-
-
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_HOE)
-                .pattern("SS ")
-                .pattern(" C ")
-                .pattern(" C ")
-                .input('S', Items.EMERALD)
-                .input('C', Items.STICK)
-
-                .criterion(FabricRecipeProvider.hasItem(Items.EMERALD),
-                        FabricRecipeProvider.conditionsFromItem(Items.EMERALD))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_HOE)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.EMERALD_HOE).pattern("SS ").pattern(" C ").pattern(" C ").input('S', Items.EMERALD).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(Items.EMERALD), FabricRecipeProvider.conditionsFromItem(Items.EMERALD)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.EMERALD_HOE)));
 
         //CITRINE
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_PICKAXE)
-                .pattern("SSS")
-                .pattern(" C ")
-                .pattern(" C ")
-                .input('S', ModItems.CITRINE)
-                .input('C', Items.STICK)
-                .criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE),
-                        FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_PICKAXE)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_PICKAXE).pattern("SSS").pattern(" C ").pattern(" C ").input('S', ModItems.CITRINE).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE), FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_PICKAXE)));
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_SWORD)
-                .pattern(" S ")
-                .pattern(" S ")
-                .pattern(" C ")
-                .input('S', ModItems.CITRINE)
-                .input('C', Items.STICK)
-                .criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE),
-                        FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_SWORD)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_SWORD).pattern(" S ").pattern(" S ").pattern(" C ").input('S', ModItems.CITRINE).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE), FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_SWORD)));
 
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_AXE)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_AXE).pattern("SS ").pattern("SC ").pattern(" C ").input('S', ModItems.CITRINE).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE), FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_AXE)));
+
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_HOE).pattern("SS ").pattern(" C ").pattern(" C ").input('S', ModItems.CITRINE).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE), FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_HOE)));
+
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_SHOVEL).pattern(" S ").pattern(" C ").pattern(" C ").input('S', ModItems.CITRINE).input('C', Items.STICK).criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE), FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE)).criterion(FabricRecipeProvider.hasItem(Items.STICK), FabricRecipeProvider.conditionsFromItem(Items.STICK)).offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_SHOVEL)));
+
+
+
+
+        //FOR FUTURE SHAPED RECIPES
+       /* ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_AXE)
                 .pattern("SS ")
                 .pattern("SC ")
                 .pattern(" C ")
@@ -142,39 +80,7 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
                         FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE))
                 .criterion(FabricRecipeProvider.hasItem(Items.STICK),
                         FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_AXE)));
-
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_HOE)
-                .pattern("SS ")
-                .pattern(" C ")
-                .pattern(" C ")
-                .input('S', ModItems.CITRINE)
-                .input('C', Items.STICK)
-                .criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE),
-                        FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_HOE)));
-
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.CITRINE_SHOVEL)
-                .pattern(" S ")
-                .pattern(" C ")
-                .pattern(" C ")
-                .input('S', ModItems.CITRINE)
-                .input('C', Items.STICK)
-                .criterion(FabricRecipeProvider.hasItem(ModItems.CITRINE),
-                        FabricRecipeProvider.conditionsFromItem(ModItems.CITRINE))
-                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
-                        FabricRecipeProvider.conditionsFromItem(Items.STICK))
-                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_SHOVEL)));
-
-
-
-
-    
-
+                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.CITRINE_AXE)));*/
 
 
 
